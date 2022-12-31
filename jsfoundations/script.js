11c02dbac3a692c795c1bde128553036bb2d884e
alzi03 <alammzishan03@gmail.com>
2022-12-31 18:20:47 -0500

    js foundations

---
+++ jsfoundations/script.js
@@ -0,0 +1,33 @@
+let name = "John";
+let admin = name;
+console.log(admin)
+
+let earth = 'earth';
+let currentUser = 'Alam';
+console.log(earth);
+console.log(currentUser);
+
+let x = 5;
+x--;
+console.log(x)
+
+let y = 123e+5;
+let z = 123e-5;
+console.log(y), console.log(z)
+x = 0.2 + 0.1
+console.log(x)
+
+y = 10
+z = 20
+x = 'The result is: ' + (y + z)
+console.log(x)
+
+x = '100'
+y = 10
+z = x - y
+console.log(typeof(z))
+
+/* Objects*/
+
+const person = {firstname:"Alam", lastname:"Zishan", age:"19", eyeColor:"brown"};
+person;
