6012abfd47e7728d5f62dad63ee588093155d9b6
alzi03 <alammzishan03@gmail.com>
2023-01-03 16:50:19 -0500

    more fcns

---
+++ js-syntax/jsfunctions/scripts.js
@@ -0,0 +1,39 @@
+const textBox = document.querySelector("#textBox");
+const output = document.querySelector("#output");
+
+textBox.addEventListener('keydown', (event) => output.textContent = `You pressed "${event.key}".`);
+
+const button = document.querySelector('#hellobutton');
+button.addEventListener('click', (event) => console.log('Hello'));
+
+const input = document.querySelector("#name")
+const yourname = document.querySelector("#yourname")
+const submit = document.querySelector('#submit')
+
+input.addEventListener('submit', (event) => {
+    if (input.value === ''){
+        yourname.textContent = 'Please enter your name'
+    }
+    else{
+        yourname.textContent = `Hello ${input.value}.`;
+    }
+})
+    
+
+
+
+
+
+
+
+
+/* Anonymous Functions
+textBox.addEventListener('keydown', function(event){
+    console.log(`You pressed "${event.key}"`)
+});
+These two do the same thing, one line in the curly brackets so not necessary, and since there's one parameter the function doesn't require parantheses around the paramter
+*/
+
+
+
+
