f38d59d7b5b25e51d28f3884529695bc900e6e8a
alzi03 <alammzishan03@gmail.com>
2023-01-03 15:35:14 -0500

    dev tools

---
+++ googledevtools/scripts.js
@@ -0,0 +1,15 @@
+// Styling with console.log
+console.log('%c C-styled', 'font-size: 30px;color:blue;')
+
+// warn
+console.warn('Warning')
+
+//error
+console.error('404 Error')
+
+//info
+console.info('The Sun could fit 1 million earths in it')
+
+//assert
+const heading = document.querySelector('h1')
+console.assert(heading.classList.contains('heading'), 'Incorrect!!')
