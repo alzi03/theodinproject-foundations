5f4c4841e197b74bc52d5651744283dce3a60990
alzi03 <alammzishan03@gmail.com>
2023-01-13 12:58:04 -0500

    final

--- calculator/script.js
+++ calculator/script.js
@@ -8,37 +8,59 @@ for (button of buttons){
 const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.']
 
 let add = (a, b) => {
-    return a + b
+    return +a + +b
 }
 let subtract = (a, b) => {
-    return a - b
+    return +a - +b
 }
 let multiply = (a, b) => {
-    return a * b
+    return +a * +b
 }
 let divide = (a, b) => {
-    return a / b
+    return Math.round((+a / +b) * 100) / 100
 }
 const symbols = {'+':add, '-':subtract, '/':divide, '*':multiply}
 
+let parts = [undefined, undefined, undefined];
 
 function expression(){
     window.addEventListener('keydown', function(e){
         const keyCode = e.keyCode
         if (!(keys.includes(keyCode))) return
-        console.log(e.key)
         const previousNumber = document.querySelector('#previousNumber')
         const currentNumber = document.querySelector('#currentNumber')
-        
+    
+        if (e.keyCode == 27){
+            parts = [0, undefined, undefined]
+            previousNumber.textContent = `= 0`
+        }
+
         
         if (keyCode==8){ // backspace
             currentNumber.textContent = currentNumber.textContent.slice(0, -1)}
-
-        if (e.key in symbols){ // operand
+        
+        if (e.key in symbols && parts[1]==undefined){ // operand
             const operation = symbols[e.key]
-            
-            console.log(operation)
+            parts[0] = currentNumber.textContent
+            parts[1] = operation
+            previousNumber.textContent = `= ${parts[0]} ${e.key}`
+            currentNumber.textContent = ''
+        }  
+        else if (e.key in symbols){
+            const operation = symbols[e.key]
+            parts[1] = operation
+            previousNumber.textContent = `= ${parts[0]} ${e.key}`
+            console.log(parts)
         }
+
+        if (keyCode == 13 && currentNumber.textContent !== ''){
+            parts[2] = currentNumber.textContent
+            const answer = parts[1](parts[0], parts[2])
+            previousNumber.textContent = `= ${answer}`
+            currentNumber.textContent = ''
+            parts = [answer, '']
+        }
+
 
         if (numbers.includes(e.key)){currentNumber.textContent += e.key} // typing numbers
     })
@@ -46,3 +68,21 @@ function expression(){
 
 expression()
 
+/*
+first round = parts[0, 1, 2]
+parts[0] should be set to 0
+parts [1] is inputted to +
+parts[2] is set to user input
+
+second round and onwards
+parts[0] should be set to 0 + input
+parts[1] is decided by user
+parts[2] is decided by user
+
+['','','']
+first input -> parts[0]
+first operation -? parts[1]
+second input -> parts[2]
+parts[0] = parts[1]
+
+*/
